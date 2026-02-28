ad20dc2dbec70e7dde47d100f07ce14fec59a115
ryanmaselli <108041970+ryanmaselli@users.noreply.github.com>
2026-02-28 18:29:49 -0500

    Update mtmoonb1.asm

--- data/wildPokemon/mtmoonb1.asm
+++ data/wildPokemon/mtmoonb1.asm
@@ -1,28 +1,28 @@
 MoonMonsB1:
 	db $0A
 	IF DEF(_RED)
-		db 8,PARAS
-		db 7,PARAS
-		db 7,PARAS
-		db 8,PARAS
-		db 9,PARAS
+		db 8,ZUBAT
+		db 7,ZUBAT
+		db 7,GEODUDE
+		db 8,GEODUDE
+		db 9,ZUBAT
 		db 10,PARAS
-		db 10,PARAS
-		db 11,PARAS
-		db 9,PARAS
-		db 9,PARAS
+		db 10,ZUBAT
+		db 11,ZUBAT
+		db 9,CLEFFA
+		db 9,GEODUDE
 	ENDC
 	IF DEF(_BLUE)
 		db 8,ZUBAT
 		db 7,ZUBAT
-		db 7,ZUBAT
-		db 8,ZUBAT
+		db 7,GEODUDE
+		db 8,GEODUDE
 		db 9,ZUBAT
-		db 10,ZUBAT
+		db 10,PARAS
 		db 10,ZUBAT
 		db 11,ZUBAT
-		db 9,ZUBAT
-		db 9,PARAS
+		db 9,CLEFFA
+		db 9,GEODUDE
 	ENDC
 	db $00
 
