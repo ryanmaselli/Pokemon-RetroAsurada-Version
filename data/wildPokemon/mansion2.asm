8a8d9cb527db77d37fa353b1706d7e3566082340
ryanmaselli <108041970+ryanmaselli@users.noreply.github.com>
2026-02-28 18:19:05 -0500

    Update mansion2.asm

--- data/wildPokemon/mansion2.asm
+++ data/wildPokemon/mansion2.asm
@@ -4,24 +4,24 @@ MansionMons2:
 		db 32,GROWLITHE
 		db 34,KOFFING
 		db 34,KOFFING
-		db 30,RATTATA
+		db 30,PONYTA
 		db 30,KOFFING
-		db 32,RATTATA
+		db 32,PONYTA
 		db 30,GRIMER
-		db 28,RATTATA
+		db 28,PONYTA
 		db 39,WEEZING
-		db 37,MUK
+		db 37,GRIMER
 	ENDC
 
 	IF DEF(_BLUE)
 		db 32,VULPIX
 		db 34,GRIMER
 		db 34,GRIMER
-		db 30,RATTATA
+		db 30,PONYTA
 		db 30,GRIMER
-		db 32,RATTATA
+		db 32,PONYTA
 		db 30,KOFFING
-		db 28,RATTATA
+		db 28,PONYTA
 		db 39,MUK
 		db 37,WEEZING
 	ENDC
