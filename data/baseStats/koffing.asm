add100c5bce4bcfaac1b1901b98eb95064c027a7
ryanmaselli <108041970+ryanmaselli@users.noreply.github.com>
2026-02-28 17:22:15 -0500

    Update koffing.asm

--- data/baseStats/koffing.asm
+++ data/baseStats/koffing.asm
@@ -12,7 +12,7 @@ db 114 ; base exp yield
 IF GEN_2_SPRITES
 INCBIN "pic/gsmon/koffing.pic",0,1
 ELSE
-INCBIN "pic/bmon/koffing.pic",0,1
+INCBIN "pic/rgmon/koffing.pic",0,1
 ENDC
 dw KoffingPicFront
 dw KoffingPicBack
