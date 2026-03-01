accca7b77159aa09809a386a329fcea9b2ebf8b4
ryanmaselli <108041970+ryanmaselli@users.noreply.github.com>
2026-02-28 21:54:10 -0500

    Update route13.asm

--- data/wildPokemon/route13.asm
+++ data/wildPokemon/route13.asm
@@ -1,31 +1,31 @@
 Route13Mons:
 	IF DEF(_RED)
 		db $14
-		db 24,ODDISH
-		db 25,PIDGEY
-		db 27,PIDGEY
+		db 24,GLOOM
+		db 25,PIDGEOTTO
+		db 27,PIDGEOTTO
 		db 24,VENONAT
-		db 22,ODDISH
-		db 26,VENONAT
-		db 26,FARFETCH_D
-		db 25,HAPPINY
-		db 28,GLOOM
-		db 30,GLOOM
-		db $00
-	ENDC
-
-	IF DEF(_BLUE)
-		db $14
-		db 24,BELLSPROUT
-		db 25,PIDGEY
-		db 27,PIDGEY
-		db 24,VENONAT
-		db 22,BELLSPROUT
+		db 22,GLOOM
 		db 26,VENONAT
 		db 26,FARFETCH_D
 		db 25,HAPPINY
 		db 28,WEEPINBELL
 		db 30,WEEPINBELL
+		db $00
+	ENDC
+
+	IF DEF(_BLUE)
+		db $14
+		db 24,WEEPINBELL
+		db 25,PIDGEOTTO
+		db 27,PIDGEOTTO
+		db 24,VENONAT
+		db 22,WEEPINBELL
+		db 26,VENONAT
+		db 26,FARFETCH_D
+		db 25,HAPPINY
+		db 28,GLOOM
+		db 30,GLOOM
 		db $00
 	ENDC
 
