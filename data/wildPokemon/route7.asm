7a8eaddaad6b29e2c5792edf008188365d750075
ryanmaselli <108041970+ryanmaselli@users.noreply.github.com>
2026-02-28 22:33:43 -0500

    Update route7.asm

--- data/wildPokemon/route7.asm
+++ data/wildPokemon/route7.asm
@@ -2,11 +2,11 @@ Route7Mons:
 	db $0F
 
 	IF DEF(_RED)
-		db 19,PIDGEY
+		db 19,PIDGEOTTO
 		db 19,ODDISH
 		db 17,MANKEY
-		db 22,ODDISH
-		db 22,PIDGEY
+		db 22,GLOOM
+		db 22,PONYTA
 		db 18,MANKEY
 		db 18,GROWLITHE
 		db 20,GROWLITHE
@@ -15,11 +15,11 @@ Route7Mons:
 	ENDC
 
 	IF DEF(_BLUE)
-		db 19,PIDGEY
+		db 19,PIDGEOTTO
 		db 19,BELLSPROUT
 		db 17,PANCHAM
-		db 22,BELLSPROUT
-		db 22,PIDGEY
+		db 22,WEEPINBELL
+		db 22,PONYTA
 		db 18,PANCHAM
 		db 18,VULPIX
 		db 20,VULPIX
